24de7b90851e3f997732bcdbb3e56fc747ec3697
Himanshu Singh <121339915+HimanshuGitCode@users.noreply.github.com>
2023-12-01 15:14:48 +0530

    Update Features.dart

--- lib/Client/home/Features.dart
+++ lib/Client/home/Features.dart
@@ -1,5 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:codebooter_study_app/AppState.dart';
+
 import 'package:codebooter_study_app/widgets/BigText.dart';
 import 'package:flutter/material.dart';
 import 'package:codebooter_study_app/utils/Colors.dart';
